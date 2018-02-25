7ee4820a8d54f1b2d0494947e4873d1fe8bd385d
Ilkwon Sim <simsim0709@gmail.com>
2018-02-26 00:04:51 +0900

    Add sample design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import Main from './Main';
-
+import {
+  SpeechRecognition,
+  SpeechGrammarList,
+  SpeechRecognitionEvent,
+} from './speech';
 import './App.css';
 
 class App extends Component {
+  async componentDidMount() {
+    this.recognition = new SpeechRecognition();
+    this.recognition.lang = 'ko-KR';
+    this.recognition.continuous = false;
+    this.recognition.interimResults = false;
+    this.recognition.maxAlternatives = 1;
+
+    console.log('this.recognition', this.recognition);
+
+    this.recognition.onresult = event => {
+      console.log('event.results', event.results);
+    };
+
+    this.recognition.onspeechend = () => {
+      console.log('stopped');
+      this.recognition.stop();
+    };
+
+    this.recognition.onnomatch = event => {
+      console.log('no match');
+    };
+  }
+
+  start = () => {
+    console.log('start');
+    this.recognition.start();
+  };
+
+  stop = () => {
+    console.log('stop');
+    this.recognition.stop();
+  };
+
   render() {
     return (
       <div className="demo-1">
-        <Main />
+        <Main onStart={this.start} onStop={this.stop} />
       </div>
     );
   }
